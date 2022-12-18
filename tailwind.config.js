1ae64cb408fcb800b2b5e94dd7c8ac65ae531768
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2022-12-19 01:54:23 +0300

    Feat (card-top-section): return files config

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,14 +36,7 @@ module.exports = {
 				'7xl': '5.17rem',
 				'8xl': '6.9rem',
 				'9xl': '9.2rem',
-				'default':'496px'
 			},
-			width:{
-				'cardDefault':'496px'
-			},
-			borderRadius:{
-				'cardDefault':'20px'
-			}
 		}
 	},
 	plugins: []
