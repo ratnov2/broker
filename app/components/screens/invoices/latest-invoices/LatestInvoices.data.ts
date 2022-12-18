b9aa4d1080195cadaa5d5177dad97ac8a5b3231d
Sem <semenburov@gmail.com>
2022-12-18 21:44:47 +0200

    fix fakeData + lower case folder + paggintaion functional adde (problem when change page)

--- app/components/screens/invoices/latest-invoices/LatestInvoices.data.ts
+++ app/components/screens/invoices/latest-invoices/LatestInvoices.data.ts
@@ -12,7 +12,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Tony Soap',
 		email: 'soap@mail.com',
-		invoiceNum: 'INV-001-123456',
+		invoiceNum: 'INV-002-123456',
 		service: 'Cleaning Service',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Unpaid'
@@ -21,7 +21,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Johnny Ahmad',
 		email: 'ahmad@mail.com',
-		invoiceNum: 'INV-001-123456',
+		invoiceNum: 'INV-003-123456',
 		service: 'Web Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Paid'
@@ -30,7 +30,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Karen Hope',
 		email: 'hope@mail.com',
-		invoiceNum: 'INV-001-123456',
+		invoiceNum: 'INV-004-123456',
 		service: 'Server Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Pending'
@@ -39,7 +39,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Nella Vita',
 		email: 'nella@mail.com',
-		invoiceNum: 'INV-001-123456',
+		invoiceNum: 'INV-005-123456',
 		service: 'Server Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Paid'
@@ -49,7 +49,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'William White',
 		email: 'william@mail.com',
-		invoiceNum: 'INV-002-123456',
+		invoiceNum: 'INV-006-123456',
 		service: 'Server Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Sent'
@@ -58,7 +58,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Tony Stark',
 		email: 'stark@mail.com',
-		invoiceNum: 'INV-003-123456',
+		invoiceNum: 'INV-007-123456',
 		service: 'Cleaning Service',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Unpaid'
@@ -67,7 +67,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Ahmad Teanto',
 		email: 'teanto@mail.com',
-		invoiceNum: 'INV-001-123456',
+		invoiceNum: 'INV-008-123456',
 		service: 'Web Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Paid'
@@ -76,7 +76,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Karen Black',
 		email: 'karen@mail.com',
-		invoiceNum: 'INV-005-123456',
+		invoiceNum: 'INV-009-123456',
 		service: 'Server Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Pending'
@@ -85,7 +85,7 @@ export const fakeLatestInvoices = [
 		recipientAvatar: '',
 		name: 'Vita Merci',
 		email: 'merci@mail.com',
-		invoiceNum: 'INV-009-123456',
+		invoiceNum: 'INV-010-123456',
 		service: 'Server Maintenance',
 		date: '2012-01-26T13:51:50.417-07:00',
 		status: 'Paid'
