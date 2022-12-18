f9d80c541a79d21d5cb55e8cddf0eacd91acbc48
Sem <semenburov@gmail.com>
2022-12-19 01:27:15 +0200

    pagination count showing items

--- app/components/ui/pagination/Pagination.tsx
+++ app/components/ui/pagination/Pagination.tsx
@@ -1,8 +1,11 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react';
 
-import styles from './Pagination.module.scss'
-import PaginationArrow from './pagination-arrow/PaginationArrow'
-import PaginationButton from './pagination-button/PaginationButton'
+
+
+import styles from './Pagination.module.scss';
+import PaginationArrow from './pagination-arrow/PaginationArrow';
+import PaginationButton from './pagination-button/PaginationButton';
+
 
 interface IPagination {
 	itemsPerPage: number
@@ -21,10 +24,24 @@ const Pagination: FC<IPagination> = ({
 		pageNumbers.push(i)
 	}
 
+	const showingMax =
+		currentPage * itemsPerPage > totalItems
+			? totalItems
+			: currentPage * itemsPerPage < itemsPerPage
+			? itemsPerPage
+			: currentPage * itemsPerPage
+
+	const showingMin = currentPage * itemsPerPage - (itemsPerPage - 1) //1.6.11.16
+
+	
 	return (
 		<div className={styles.pagination}>
 			<p>
-				Showing <span>1-5</span> from <span>{totalItems}</span> invoices
+				Showing{' '}
+				<span>
+					{showingMin}-{showingMax}
+				</span>{' '}
+				from <span>{totalItems}</span> invoices
 			</p>
 			<div>
 				<PaginationArrow
