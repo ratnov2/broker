2e369a810b2a1be6616639268faea1dfaa9664e8
RL <heizzman@gmail.com>
2022-12-20 21:08:56 +0100

    Fix (transaction detail): add start TransactionDetail

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Dashboard from '@/screens/dashboard/Dashboard'
+import TransactionDetail from '@/screens/transaction-detail/TransactionDetail'
 
 export default function Home() {
-	return <Dashboard />
-		
+	//return <Dashboard />
+
+	return <TransactionDetail {...{ clientId: '100', project: '#1', date: new Date }} />
 }
