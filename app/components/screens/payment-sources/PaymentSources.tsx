3294da2c884073f4367c332528a252776f99ce02
Sem <semenburov@gmail.com>
2022-12-26 11:40:28 +0200

    fix code after review

--- app/components/screens/payment-sources/PaymentSources.tsx
+++ app/components/screens/payment-sources/PaymentSources.tsx
@@ -17,25 +17,21 @@ const PaymentSources: FC = () => {
 
 	return (
 		<Layout title='Payment Sources'>
-			<div className='h-full w-full  mt-12'>
+			<div className='h-full w-full mt-12'>
 				<PaymentSourcesTopTitle />
-				<div className='flex flex-wrap justify-between'>
-					{isLoadingPaymentSources ? (
-						<Loader />
-					) : paymentSources?.length ? (
-						<>
-							{paymentSources.map(paymentContact => (
-								<PaymentContact
-									paymentContact={paymentContact}
-								/>
-							))}
-						</>
-					) : (
-						<div className='h-[225px] flex items-center justify-center'>
-							Payment contacts not found
-						</div>
-					)}
-				</div>
+				{isLoadingPaymentSources ? (
+					<Loader />
+				) : paymentSources?.length ? (
+					<div className='flex flex-wrap justify-between'>
+						{paymentSources.map(paymentContact => (
+							<PaymentContact paymentContact={paymentContact} />
+						))}
+					</div>
+				) : (
+					<div className='h-[225px] flex items-center justify-center'>
+						Payment contacts not found
+					</div>
+				)}
 			</div>
 		</Layout>
 	)
