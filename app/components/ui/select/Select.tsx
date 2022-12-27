0a651561bedf11176dd531888e8f1a3344d58130
dmytro <dimitritkach@gmail.com>
2022-12-27 18:10:48 +0100

    feat(ui-kit): refactoring

--- app/components/ui/select/Select.tsx
+++ app/components/ui/select/Select.tsx
@@ -1,77 +1,22 @@
-import cn from 'clsx'
 import { FC, useId } from 'react'
 import ReactSelect from 'react-select'
 
+import { SelectClassNames } from '@/ui/select/select.class-names'
 import { ISelect, ISelectOption } from '@/ui/select/select.interface'
 
-import styles from './Select.module.scss'
-
 const Select: FC<ISelect<ISelectOption, boolean>> = ({
-	// classNamePrefix = 'select',
 	variant = 'primary',
 	color = 'purple',
 	size = 'sm',
+	classNames = SelectClassNames(variant, color, size),
 	...rest
 }) => {
 	return (
 		<ReactSelect
-			classNames={{
-				control: state =>
-					state.isFocused
-						? cn(
-								styles.control__focused,
-								styles[color],
-								styles[size],
-								styles[variant]
-						  )
-						: cn(
-								styles.control,
-								styles[color],
-								styles[size],
-								styles[variant]
-						  ),
-				dropdownIndicator: state =>
-					state.isFocused
-						? cn(
-								styles.dropdownIndicator__focused,
-								styles[color],
-								styles[variant]
-						  )
-						: cn(
-								styles.dropdownIndicator,
-								styles[color],
-								styles[variant]
-						  ),
-				valueContainer: state =>
-					cn(styles.valueContainer, styles[variant]),
-				placeholder: state =>
-					state.isFocused
-						? cn(styles.placeholder, styles[variant])
-						: cn(styles.placeholder, styles[variant]),
-				singleValue: state => cn(styles.singleValue, styles[variant]),
-				input: state => cn(styles.Input, styles[variant]),
-				indicatorSeparator: state =>
-					cn(styles.indicatorSeparator, styles[variant]),
-				indicatorsContainer: state =>
-					cn(styles.indicatorsContainer, styles[variant]),
-				// options
-
-				menu: state =>
-					cn(
-						styles.menu,
-						styles[color],
-						styles[size],
-						styles[variant]
-					),
-				menuList: state => cn(styles.menuList, styles[variant]),
-				option: state =>
-					state.isSelected
-						? cn(styles.Option__selected, styles[variant])
-						: cn(styles.Option, styles[variant])
-			}}
+			classNames={classNames}
 			instanceId={useId()}
-			// menuIsOpen={true}
 			{...rest}
+			// menuIsOpen={true} // needed for inspect in browser
 		/>
 	)
 }
--- app/components/ui/select/select.interface.ts
+++ app/components/ui/select/select.interface.ts
@@ -1,4 +1,4 @@
-import { GroupBase, Props } from 'react-select'
+import { ClassNamesConfig, GroupBase, Props } from 'react-select'
 
 import { ISize, ITailwindColors, IVariant } from '@/ui/ui.interface'
 
@@ -7,8 +7,7 @@ export interface ISelect<
 	IsMulti extends boolean,
 	Group extends GroupBase<Option> = GroupBase<Option>
 > extends Props<Option, IsMulti, Group> {
-	// here you can extend react-select interface
-	// propsName : type
+	// here you can extend react-select interface with other custom props
 	color?: ITailwindColors
 	size?: ISize
 	variant?: IVariant
@@ -18,3 +17,11 @@ export interface ISelectOption {
 	value: string
 	label: string
 }
+
+export type ISelectClassNames = (
+	variant: IVariant,
+	color: ITailwindColors,
+	size: ISize
+) =>
+	| ClassNamesConfig<ISelectOption, boolean, GroupBase<ISelectOption>>
+	| undefined
