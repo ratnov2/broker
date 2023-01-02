3b38c6a55e297e1b848cf6a9bda4585d8632f66b
Sem <semenburov@gmail.com>
2023-01-02 17:23:24 +0200

    merge auth

--- app/api/interceptors.ts
+++ app/api/interceptors.ts
@@ -4,7 +4,6 @@ import Cookies from 'js-cookie'
 import { getNewTokens } from '@/providers/auth/helper.auth'
 
 import { API_URL } from '@/config/api.config'
-//import { IS_PRODUCTION } from '@/config/constants'
 
 import { errorCatch, getContentType } from './api.helpers'
 import { removeTokensStorage } from '@/services/auth/auth.helper'
@@ -23,7 +22,6 @@ instance.interceptors.request.use(config => {
 	const accessToken = Cookies.get('accessToken')
 
 	if (config.headers && accessToken) {
-		// config.headers.Authorization = `Bearer ${accessToken}`
 		config.headers = { Authorization: `Bearer ${accessToken}` }
 	}
 
