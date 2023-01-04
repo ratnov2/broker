647594a5ce90857e29df758db919b0c31c86b53c
dmytro <dimitritkach@gmail.com>
2023-01-04 14:56:12 +0100

    feat(auth): replaced react-query with @tanstack/react-query

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -1,5 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { FC, PropsWithChildren } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import AuthProvider from './auth/AuthProvider'
 
