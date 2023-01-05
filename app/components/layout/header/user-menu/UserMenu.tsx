64f4fabc3b86e7d83c2d2ae799281f7226e769a6
dmytro <dimitritkach@gmail.com>
2023-01-05 17:09:34 +0100

    feat(header profile data)

--- app/components/layout/header/user-menu/UserMenu.tsx
+++ app/components/layout/header/user-menu/UserMenu.tsx
@@ -4,22 +4,27 @@ import { FC } from 'react'
 
 import Logout from '@/layout/header/user-menu/logout/Logout'
 
+import { useProfile } from '@/hooks/useProfile'
+
 import styles from './UserMenu.module.scss'
 import UserLinks from './user-links/UserLinks'
 
 const UserMenu: FC = () => {
+	const { data: profile } = useProfile()
+
 	return (
 		<nav>
 			<Logout />
 			<UserLinks />
 			<Link href='/profile' className={styles.profile}>
 				<Image
-					src='/images/avatar-empty.png'
+					className={styles.profile_image}
+					src={profile?.avatarPath || '/images/avatar-empty.png'}
 					width={56}
 					height={56}
 					alt='profile'
 				/>
-				<span>Rara Avis</span>
+				<span>{profile?.name}</span>
 			</Link>
 		</nav>
 	)
