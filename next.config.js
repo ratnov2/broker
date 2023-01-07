cf28d19e9de8c1d0e5a966ceffc2d89ad40e38fc
Sem <semenburov@gmail.com>
2023-01-07 13:18:02 +0200

    merge auth

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cloudflare-ipfs.com',
+				port: '',
+				pathname: '/ipfs/**'
+			}
+		]
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `https://red-project-bank-app.herokuapp.com/api/:path*`
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
