98c6dcef3d99658e354ce8e662d53bd2c2dffc3a
Sem <semenburov@gmail.com>
2023-01-11 21:45:26 +0200

    finished refactoring

--- app/components/screens/cards/cards-list/CardsList.tsx
+++ app/components/screens/cards/cards-list/CardsList.tsx
@@ -1,80 +1,13 @@
-import { FC, useState } from 'react';
+import { FC, useState } from 'react'
 
+import Loader from '@/ui/loader/Loader'
+import Pagination from '@/ui/pagination/Pagination'
 
+import { useCards } from '@/hooks/useCards'
+import { useUserProfile } from '@/hooks/useUserProfile'
 
-import Loader from '@/ui/loader/Loader';
-import Pagination from '@/ui/pagination/Pagination';
-
-
-
-import { useCards } from '@/hooks/useCards';
-
-
-
-import styles from './CardsList.module.scss';
-import CardRow from '@/screens/cards/cards-list/CardRow';
-import { useUserProfile } from '@/hooks/useUserProfile';
-
-
-type TypeCards = {
-	type: 'Primary' | 'Secondary'
-	bankName: string
-	holderName: string
-	cardNumber: string
-	validity: string
-	active: boolean
-}
-
-const cards: TypeCards[] = [
-	{
-		type: 'Primary',
-		bankName: 'Bank ABC',
-		holderName: 'Rara Avis',
-		cardNumber: '•••• •••• •••• 1235',
-		validity: '03/21',
-		active: true
-	},
-	{
-		type: 'Secondary',
-		bankName: 'Bank DEF',
-		holderName: 'Rara Avis',
-		cardNumber: '•••• •••• •••• 1236',
-		validity: '03/21',
-		active: false
-	},
-	{
-		type: 'Secondary',
-		bankName: 'Bank HIJ',
-		holderName: 'Rara Avis',
-		cardNumber: '•••• •••• •••• 1237',
-		validity: '03/21',
-		active: true
-	},
-	{
-		type: 'Primary',
-		bankName: 'Bank ABC',
-		holderName: 'Rara Avis',
-		cardNumber: '•••• •••• •••• 1238',
-		validity: '03/21',
-		active: true
-	},
-	{
-		type: 'Secondary',
-		bankName: 'Bank DEF',
-		holderName: 'Rara Avis',
-		cardNumber: '•••• •••• •••• 1239',
-		validity: '03/21',
-		active: false
-	},
-	{
-		type: 'Secondary',
-		bankName: 'Bank HIJ',
-		holderName: 'Rara Avis',
-		cardNumber: '•••• •••• •••• 1224',
-		validity: '03/21',
-		active: true
-	}
-]
+import styles from './CardsList.module.scss'
+import CardRow from '@/screens/cards/cards-list/CardRow'
 
 const CardList: FC = () => {
 	const [currentPage, setCurrentPage] = useState<number>(1)
--- app/services/cards/cards.service.ts
+++ app/services/cards/cards.service.ts
@@ -1,11 +1,29 @@
-import { axiosClassic } from 'api/interceptors'
+import { instance } from 'api/interceptors'
 
 import { ICard } from '@/shared/types/card.types'
 
 import { getAccountsUrl } from '@/config/api.config'
 
+//import { request } from '@/api/request.api'
+
 export const CardService = {
 	async getUserCards(page = 1, perPage = 5) {
-		return axiosClassic.get<ICard[]>(getAccountsUrl(`/get-user-cards`, page, perPage))
+		return instance.get<ICard[]>(
+			getAccountsUrl(`/get-user-cards`, page, perPage)
+		)
 	}
 }
+
+/* export const CardService2 = {
+	async getUserCards(page = 1, perPage = 5) {
+		const response = await request<ICard[]>({
+			url: getAccountsUrl(`/get-user-cards`, page, perPage),
+			method: 'GET',
+			data: { page, perPage }
+		})
+
+		console.log(JSON.stringify(response))
+			
+		return response
+	}
+} */
