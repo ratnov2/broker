a61bbf36f59d34ef6d9810bc88768d8f86c95095
dmytro <dimitritkach@gmail.com>
2023-01-21 09:00:35 +0100

    temp with error

--- app/api/request.api.ts
+++ app/api/request.api.ts
@@ -2,7 +2,7 @@ import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 import { instance } from './interceptors'
 
-export const request = async <T>(config: AxiosRequestConfig<T>) => {
+export const request = async <T>(config: AxiosRequestConfig) => {
 	const onSuccess = (response: AxiosResponse<T>) => response.data
 
 	const onError = (error: AxiosError<T>) => Promise.reject(error)
