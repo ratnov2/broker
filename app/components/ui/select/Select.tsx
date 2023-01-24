84cf027e4d8bccf8453aa6a7a88b29e95c5e89ed
dmytro <dimitritkach@gmail.com>
2023-01-24 08:19:43 +0100

    fix forwardRef in Select component

--- app/components/ui/select/Select.tsx
+++ app/components/ui/select/Select.tsx
@@ -1,24 +1,27 @@
-import { FC, useId } from 'react'
+import { FC, forwardRef, useId } from 'react'
 import ReactSelect from 'react-select'
 
 import { selectClassNames } from '@/ui/select/select.class-names'
 import { ISelect, ISelectOption } from '@/ui/select/select.interface'
 
-const Select: FC<ISelect<ISelectOption, boolean>> = ({
-	variant = 'primary',
-	color = 'purple',
-	size = 'sm',
-	classNames = selectClassNames(variant, color, size),
-	...rest
-}) => {
-	return (
-		<ReactSelect
-			classNames={classNames}
-			instanceId={useId()}
-			{...rest}
-			// menuIsOpen={true} // needed for inspect in browser
-		/>
-	)
-}
+const Select: FC<ISelect<ISelectOption, boolean>> = forwardRef(
+	({
+		variant = 'primary',
+		color = 'purple',
+		size = 'sm',
+		classNames = selectClassNames(variant, color, size),
+		...rest
+	}, ref:any) => {
+		return (
+			<ReactSelect
+				ref={ref}
+				classNames={classNames}
+				instanceId={useId()}
+				{...rest}
+				// menuIsOpen={true} // needed for inspect in browser
+			/>
+		)
+	}
+)
 
 export default Select
